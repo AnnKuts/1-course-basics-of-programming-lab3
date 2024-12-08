14df569a48715bb7fa86d844a9435b7ff558fa87
Ann Kuts <annkuts@Anns-MacBook-Pro.local>
2024-12-08 22:52:30 +0200

    made 4 task

--- Exercises/4-methods.js
+++ Exercises/4-methods.js
@@ -1,21 +1,12 @@
-'use strict';
+"use strict";
 
 const methods = iface => {
-  // Introspect all properties of iface object and
-  // extract function names and number of arguments
-  // For example: {
-  //   m1: (x) => [x],
-  //   m2: function (x, y) {
-  //     return [x, y];
-  //   },
-  //   m3(x, y, z) {
-  //     return [x, y, z];
-  //   }
-  // will return: [
-  //   ['m1', 1],
-  //   ['m2', 2],
-  //   ['m3', 3]
-  // ]
+  const resultIHateTests = [];
+  for (const key in iface) {
+    if (typeof iface[key] === "function") {
+      resultIHateTests.push([key, iface[key].length]);
+    }
+  }
+  return resultIHateTests;
 };
-
 module.exports = { methods };
